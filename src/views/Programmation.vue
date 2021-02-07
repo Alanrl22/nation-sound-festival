35e6a01df2db514a6cc1a5844280e120dc7a07fe
Violette <violette.hurel@gmail.com>
2021-02-07 13:17:23 +0100

    Make programmation filters great again

--- src/views/Programmation.vue
+++ src/views/Programmation.vue
@@ -7,12 +7,12 @@
           <strong>Tous</strong>
         </b-dropdown-item>
         <b-dropdown-item
-          v-for="(card, index) in info"
-          :key="index"
-          :value="card.day"
-          @click="selectedType = card.day"
+          v-for="(day, index) in days"
+          :key="`day-${index}`"
+          :value="day"
+          @click="selectedType = day"
         >
-          {{ card.day }}
+          {{ day }}
         </b-dropdown-item>
       </b-dropdown>
       <b-dropdown text="Heure" variant="outline-danger">
@@ -20,12 +20,12 @@
           <strong>Tous</strong>
         </b-dropdown-item>
         <b-dropdown-item
-          v-for="(card, index) in info"
-          :key="index"
-          :value="card.hour"
-          @click="selectedType = card.hour"
+          v-for="(hour, index) in hours"
+          :key="`jour-${index}`"
+          :value="hour"
+          @click="selectedType = hour"
         >
-          {{ card.hour }} Heures
+          {{ hour }}h
         </b-dropdown-item>
       </b-dropdown>
 
@@ -34,12 +34,12 @@
           <strong>Tous</strong>
         </b-dropdown-item>
         <b-dropdown-item
-          v-for="(card, index) in info"
-          :key="index"
-          :value="card.type"
-          @click="selectedType = card.type"
+          v-for="(type, index) in types"
+          :key="`type-${index}`"
+          :value="type"
+          @click="selectedType = type"
         >
-          {{ card.type }}
+          {{ type }}
         </b-dropdown-item>
       </b-dropdown>
 
@@ -48,12 +48,12 @@
           <strong>Tous</strong>
         </b-dropdown-item>
         <b-dropdown-item
-          v-for="(card, index) in info"
-          :key="index"
-          :value="card.stage"
-          @click="selectedType = card.stage"
+          v-for="(stage, index) in stages"
+          :key="`stage-${index}`"
+          :value="stage"
+          @click="selectedType = stage"
         >
-          {{ card.stage }}
+          {{ stage }}
         </b-dropdown-item>
       </b-dropdown>
     </b-row>
@@ -61,8 +61,8 @@
     <div class="container-fluid d-flex flex-wrap">
       <div
         class="card_prog col-md-6 col-xl-4"
-        v-for="(card, index) in info"
-        :key="index"
+        v-for="(card, index) in filteredCategory"
+        :key="`card-${index}`"
       >
         <div class="card_title">
           <h3>{{ card.concert }}</h3>
@@ -84,58 +84,53 @@
 <script>
 import axios from "axios";
 
+let hours = ["15", "16", "17", "18", "19", "20", "21", "22", "23", "00", "01"];
+let days = ["Vendredi", "Samedi", "Dimanche"];
+
 export default {
   data() {
     return {
-      info: null,
+      cards: null,
+      types: null,
+      days: days,
+      hours: hours,
+      stages: null,
+      selectedType: "All",
     };
   },
   mounted() {
     axios
       .get("https://nation-sound.herokuapp.com/api/lineup")
-      .then((response) => (this.info = response.data))
+      .then((response) => (this.cards = response.data))
+      .catch((error) => console.log(error));
+    axios
+      .get("https://nation-sound.herokuapp.com/api/stages")
+      .then((response) => (this.stages = response.data))
+      .catch((error) => console.log(error));
+    axios
+      .get("https://nation-sound.herokuapp.com/api/musicstyles")
+      .then((response) => (this.types = response.data))
       .catch((error) => console.log(error));
   },
-
-  // created() {
-  //   this.fetchData();
-  // },
-  // methods: {
-  //   // Récupère les données de l'API
-  //   fetchData: function() {
-  //     let self = this;
-
-  //     axios
-  //       .get("https://nation-sound.herokuapp.com/api/lineup")
-  //       .then(function(response) {
-  //         // Datas pronvenant de l'API
-  //         console.log(response.data);
-  //         // Anciennes Data (qui va jarter)
-  //         console.log(self.cards);
-  //         // Nouvelle Data remplace l'ancienne
-  //         self.cards = response.data;
-  //       })
-  //       .catch(function(error) {
-  //         console.log(error);
-  //       });
-  //   },
-  // },
   computed: {
     filteredCategory: function() {
       let self = this;
       let cardsArray = self.cards;
       let selectedType = self.selectedType;
+
       if (selectedType === "All") {
         return cardsArray;
       } else {
         cardsArray = cardsArray.filter(function(card) {
+          var n = card.hour.toString();
           if (
             card.type.indexOf(selectedType) !== -1 ||
             card.day.indexOf(selectedType) !== -1 ||
-            card.hour.indexOf(selectedType) !== -1 ||
+            n.indexOf(selectedType) !== -1 ||
             card.stage.indexOf(selectedType) !== -1
           ) {
-            return card.orderBy(this.bigArtists, 0);
+            // return card.orderBy(self.bigArtist, 0);
+            return card;
           }
         });
       }
@@ -213,7 +208,9 @@ export default {
   overflow: hidden;
   position: relative;
 
-  &:hover, &:active, &:focus {
+  &:hover,
+  &:active,
+  &:focus {
     p {
       opacity: 1;
     }
@@ -240,5 +237,9 @@ export default {
   width: 100%;
   opacity: 1;
   transition: 0.3s;
+}
+
+.p-list {
+  margin: 0;
 }
 </style>
